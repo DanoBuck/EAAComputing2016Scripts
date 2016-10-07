2c0cbb190ce8532682cc51d974defc6a0fd8b93d
DanoBuck <DaniielBuckleyTY3@gmail.com>
2016-10-07 10:13:49 +0100

    Script now takes paramters and prints to command line

--- Week1/firstscript.sh
+++ Week1/firstscript.sh
@@ -10,5 +10,5 @@ mkdir Week2
 cd Week2
 mkdir subdir1 subdir2 subdir3
 
-#a alternatively
+# alternatively
 mkdir Week2/subdir1 Week2/subdir2 Week2/subdir3
--- Week2/ScriptForLab2.sh
+++ Week2/ScriptForLab2.sh
@@ -1,3 +1,7 @@
-#!/bash/bin
+#/bin/bash
 
 echo "This will print parameters passed in"
+
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
+echo "Parameter 3: $3"
