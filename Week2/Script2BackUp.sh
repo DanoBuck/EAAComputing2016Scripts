295cf3655c2efc6efe1d33b3319efcd2a32a7882
DanoBuck <DaniielBuckleyTY3@gmail.com>
2016-10-07 10:55:01 +0100

    Scripts for lab

---
+++ Week2/Script2BackUp.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+echo "Backup Script"
+echo "$date +"%m")"
+# delete the directory first if it exists
+rmdir /home/eaauser/Backup_$date
+# throws an exception if Backup already exists
+mkdir /home/eaauser/Backup_$date
+
+cp -avr /home/eaauser/Documents /home/eaauser/Backup_$date
+
+echo "Documents are now backed up"
+
+
--- Week2/ScriptForLab2.sh
+++ Week2/ScriptForLab2.sh
@@ -2,6 +2,11 @@
 
 echo "This will print parameters passed in"
 
+# Part 1
 echo "Parameter 1: $1"
 echo "Parameter 2: $2"
 echo "Parameter 3: $3"
+
+echo "Parameter number : $#"
+
+echo "Is this correct?"
