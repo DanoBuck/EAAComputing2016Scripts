6f0eaf345f4ef844e989b6a8e5e87bd9ab081a43
DanoBuck <DaniielBuckleyTY3@gmail.com>
2016-10-14 10:03:37 +0100

    Files have been modified to parameterise the diary file name

--- Week4/diary.sh
+++ Week4/diary.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+diaryFile=Diary.txt
 echo "Diary.sh Script"
 
 read diaryLine
 
-echo $( date ) $diaryLine >> Diary.txt
+echo $( date ) $diaryLine >> $diaryFile
+
 #echo alternative 'date' $diaryLine >> Diary.txt
